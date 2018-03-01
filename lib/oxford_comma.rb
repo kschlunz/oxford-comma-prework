c2d105e17ebcdd9498495c32559fa97dcdb9a33f
Kate Schlunz <schlunzk@gmail.com>
2018-03-01 14:54:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  array.join(",")
-  array.join(" and ")
-  if array.length == 3
-    array.join(", and ")
+  if array.length == 1
+    array.join(",")
   end 
+  array.join(" and ")
+  
 
 end
